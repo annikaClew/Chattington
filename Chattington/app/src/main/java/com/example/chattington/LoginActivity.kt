a20643d69d5f58957012f2da986015d88a52550c
MichaelS608 <45910436+MichaelS608@users.noreply.github.com>
2023-04-19 20:58:57 -0700

    Added all login and registration basics

--- Chattington/app/src/main/java/com/example/chattington/LoginActivity.kt
+++ Chattington/app/src/main/java/com/example/chattington/LoginActivity.kt
@@ -1,20 +1,80 @@
 package com.example.chattington
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Text
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         val loginButton = findViewById<Button>(R.id.btnLogin)
+        val registerLink = findViewById<TextView>(R.id.textViewNewUser)
         val intent = Intent(this, MainActivity::class.java)
+        val intent2 = Intent(this, RegistrationActivity::class.java)
 
         loginButton.setOnClickListener {
             startActivity(intent)
+        }
+
+        registerLink.setOnClickListener {
+            startActivity(intent2)
+        }
+
+        findViewById<Button>(R.id.btnLogin).setOnClickListener {
+
+            val email = findViewById<EditText>(R.id.et_EmailInput).text.toString()
+            val password = findViewById<EditText>(R.id.et_PasswordInput).text.toString()
+
+            auth = FirebaseAuth.getInstance()
+            val db = Firebase.firestore
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(baseContext, "Email and password cannot be empty", Toast.LENGTH_LONG)
+                    .show()
+                return@setOnClickListener
+            }
+
+            if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                Toast.makeText(baseContext, "Invalid email format", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            //querying db for users with matching email
+            val docRef = db.collection("users").whereEqualTo("email", email)
+            docRef.get()
+                .addOnSuccessListener { documents -> //if the document exists
+                    for (document in documents) { //loop through all of the documents
+                        val password2 =
+                            document.getString("password") //get password for that document
+                        if (password != null && password.equals(password2)) //if password matches the one user entered
+                        {
+                            Log.d(TAG, "signInWithEmail:success") //log them in
+                            startActivity(Intent(this, MainActivity::class.java))
+                        } else //wrong password
+                        {
+                            Toast.makeText(baseContext, "Incorrect password", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.d(TAG, "get failed with ", exception)
+                    Toast.makeText(baseContext, "Account not found", Toast.LENGTH_LONG).show()
+                }
+
         }
     }
 }
--- Chattington/app/src/main/java/com/example/chattington/RegistrationActivity.kt
+++ Chattington/app/src/main/java/com/example/chattington/RegistrationActivity.kt
@@ -1,11 +1,70 @@
 package com.example.chattington
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_registration)
+
+
+        findViewById<Button>(R.id.buttonRegisterAccount).setOnClickListener {
+            val email = findViewById<EditText>(R.id.et_EmailInput).text.toString()
+            val password = findViewById<EditText>(R.id.et_PasswordInput).text.toString()
+            val passwordconfirmation = findViewById<EditText>(R.id.et_ConfirmPasswordInput).text.toString()
+            val username = findViewById<EditText>(R.id.et_UsernameInput).text.toString()
+
+            if (password == passwordconfirmation) {
+                val db = FirebaseFirestore.getInstance()
+                val user: MutableMap<String, Any> = HashMap()
+                user["email"] = email
+                user["password"] = password
+                user["username"] = username
+
+                db.collection("users")
+                    .add(user)
+                    .addOnSuccessListener {
+                        Log.d("dbfirebase", "save: ${user}")
+                        val text = "You successfully created an account into the awesome app!"
+                        val toast = Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT)
+                        toast.show()
+                    }
+                    .addOnFailureListener {
+                        Log.d("dbfirebase Failed", "${user}")
+                        val text = "You were Unsucessful at creating an account"
+                        val toast = Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT)
+                        toast.show()
+                    }
+                db.collection("users")
+                    .get()
+                    .addOnCompleteListener {
+                        val result: StringBuffer = StringBuffer()
+                        if (it.isSuccessful) {
+                            for (document in it.result!!) {
+                                Log.d(
+                                    "dbfirebase", "retrieve " +
+                                            "${document.data.getValue("email")} " +
+                                            "${document.data.getValue("password")}" +
+                                            "${document.data.getValue("username")}"
+                                )
+                            }
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        }
+                    }
+            } else {
+                val text = "Passwords do not match!"
+                val toast = Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        }
     }
 }
